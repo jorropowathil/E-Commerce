864de36e5a81e3327c9f5370149ccadef35fcf39
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-04 23:07:28 -0400

    worked on cateogory routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,18 +2,32 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryInfo = await Category.FindAll({
+      include: [{model : Product}]
+    })
+    res.status(200).json(categoryInfo);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    // find one category by its `id` value
+    const categoryInfo = await Category.findByPk(req.params.id, {
+       // be sure to include its associated Products
+      include: [{model : Product}]
+    })
+    res.status(200).json(categoryInfo);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
+  const categoryInfo = await Category.craete(req.body);
   // create a new category
 });
 
