122e4ee1700bef454cdc14424279b0274b46e0a6
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-05 00:05:57 -0400

    tag routes done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,14 +34,42 @@ router.get  ('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+      tag_name: req.body.tag_name
+  })
+  .then((newTag) => {
+      res.join(newTag)
+  })
+  .catch((err) => {
+      res.json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  })
+  .then((tagUpdated) => {
+    res.join(tagUpdated)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    tag_name: req.body.tag_name
+  })
+  .then((tagDeleted) => {
+    res.join(tagDeleted)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
 module.exports = router;
+
